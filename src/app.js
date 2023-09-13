4248e092ad63c68e94f167f2a0c9480f5ad34de2
elizonRL <elizorodriguez16@gmail.com>
2023-09-13 15:18:07 -0400

    V1.2 Api crud listo

--- src/app.js
+++ src/app.js
@@ -8,20 +8,13 @@ require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const muvieRouter = require('./Router/muvie.router').router;
-<<<<<<< HEAD
-const cors = require('cors')
-=======
 const cors = require('cors');
->>>>>>> d78b774851f67ec9813ab9c35959c402a1b6c87e
 
 const port = process.env.PORT;
 const app = express();
 app.use(bodyParser.json());
-<<<<<<< HEAD
-app.use(cors())
-=======
+
 app.use(cors());
->>>>>>> d78b774851f67ec9813ab9c35959c402a1b6c87e
 
 /** 
 * Funcion que optines .
@@ -30,9 +23,6 @@ app.use(cors());
 * @return {ReturnValueDataTypeHere} Brief description of the returning value here.
 */
 app.use('/',muvieRouter);
-
-//app.use("/buscarpelicula/", muvieRuter)
-  
 
 
 app.listen(port, () => {
